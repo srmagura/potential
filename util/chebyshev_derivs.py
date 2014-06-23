10ece5a263922c1ab4fecde245816eecdf2e2ba4
Sam Magura <srmagura@gmail.com>
2014-06-23 11:05:31 -0400

    Add 1st and 3rd Chebyshev derivs

--- util/chebyshev_derivs.py
+++ util/chebyshev_derivs.py
@@ -3,11 +3,9 @@ from sympy import *
 J, t = symbols('J t')
 T = cos(J * acos(t)) 
 
-d2_T_dt2 = diff(T, t, 2)
-print('d2_T_dt2 =')
-print(d2_T_dt2)
-print()
+for n in range(1, 5):
+    dn_T_t = diff(T, t, n)
+    print('d{}_T_t ='.format(n))
+    print(dn_T_t)
+    print()
 
-d4_T_dt4 = diff(d2_T_dt2, t, 2)
-print('d4_T_dt4 =')
-print(d4_T_dt4)
