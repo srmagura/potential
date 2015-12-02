00830c9c58ad21ae61db32f10e489fe5dfcc37b2
Sam Magura <srmagura@gmail.com>
2015-12-01 19:46:37 -0500

    h2 k2

--- linop.py
+++ linop.py
@@ -13,18 +13,20 @@ def apply_L(v, order, AD_len, k):
     h = AD_len / N
 
     Lv = np.zeros((N-1, N-1), dtype=complex)
+    h2 = h**2
+    k2 = k**2
 
     for m, n in it.product(range(1, N), repeat=2):
-        Lv[m-1, n-1] = 1/h**2*(v[m+1, n] + v[m-1, n] + v[m, n+1] +
-            v[m, n-1] - 4*v[m, n]) + k**2 * v[m, n]
+        Lv[m-1, n-1] = 1/h2*(v[m+1, n] + v[m-1, n] + v[m, n+1] +
+            v[m, n-1] - 4*v[m, n]) + k2 * v[m, n]
 
         if order == 4:
-            Lv[m-1, n-1] += (1/(6*h**2) * (
+            Lv[m-1, n-1] += (1/(6*h2) * (
                 v[m+1, n+1] - 2*v[m+1, n] + v[m+1, n-1] -
                 2 * (v[m, n+1] - 2*v[m, n] + v[m, n-1]) +
                 v[m-1, n+1] - 2*v[m-1, n] + v[m-1, n-1]
                 ) +
-                k**2/12 * (v[m+1, n] - 2*v[m, n] + v[m-1, n] +
+                k2/12 * (v[m+1, n] - 2*v[m, n] + v[m-1, n] +
                 v[m, n+1] - 2*v[m, n] + v[m, n-1]))
 
     return Lv
