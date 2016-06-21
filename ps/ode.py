f023a03e61234c644dc2a4abdc7999794bf6a10e
Sam Magura <srmagura@gmail.com>
2016-06-21 15:11:34 -0400

    going to try to solve all ode's in one sweep

--- ps/ode.py
+++ ps/ode.py
@@ -6,7 +6,7 @@ from scipy.interpolate import interp1d
 import abcoef
 import fourier
 
-fourier_N = 128  # Fourier error will be approx 1e-15
+fourier_N = 16 # Fourier error will be approx 1e-15
 
 def calc_z(problem, th_data, M):
     a = problem.a
@@ -17,51 +17,48 @@ def calc_z(problem, th_data, M):
     arc_dst = lambda func: fourier.arc_dst(a, func, N=fourier_N)
 
     # Estimate the accuracy of the DFT
-    #r = 1
-    #d1 = arc_dst(lambda th: problem.eval_f_polar(r, th))[:M]
-    #d2 = fourier.arc_dst(a, lambda th: problem.eval_f_polar(r, th), N=2048)[:M]
-    #print('Fourier error:', np.max(np.abs(d1-d2)))
+    r = R
+    d1 = arc_dst(lambda th: problem.eval_f_polar(r, th))[:M]
+    d2 = fourier.arc_dst(a, lambda th: problem.eval_f_polar(r, th), N=2048)[:M]
+    print('Fourier error:', np.max(np.abs(d1-d2)))
 
     h = R / 2048
-    r_data = [0, h] + [problem.boundary.eval_r(th) for th in th_data]
-    r_data += list(np.linspace(h, R, 256)[1:])
-    r_data.sort()
-    r_data = np.array(r_data)
+    r_data = np.array([0, h] + [problem.boundary.eval_r(th) for th in th_data])
 
     f_fourier_cache = {}
-    z_fourier = {}
 
-    rth_map = {}
-    for th in th_data:
-        z_fourier[th] = np.zeros(M)
-
-        for i in range(r_data):
-            if boundary.eval_r(th) == r_data[i]:
-                rth_map[th] = r_data[i]
-
+    z_fourier = np.zeros((len(th_data), M))
+    global n
+    n= 0
     for m in range(1, M+1):
         def eval_deriv(Y, r):
+            global n
             assert r != 0
 
             z1 = Y[0]
             z2 = Y[1]
 
             if r not in f_fourier_cache:
+                n += 1
                 f_fourier_cache[r] = arc_dst(lambda th: problem.eval_f_polar(r, th))[:M]
 
             d_z1_r = z2
             d_z2_r = -z2/r - (k**2 - (m*nu/r)**2) * z1 + f_fourier_cache[r][m-1]
             return np.array([d_z1_r, d_z2_r])
 
-        odeint_options = {'atol': 1e-14}
-        sol = odeint(eval_deriv, [0, 0], r_data[1:], **odeint_options).T
+        sol = odeint(eval_deriv, [0, 0], r_data[1:],
+            atol=1e-12, rtol=1e-12).T
 
-        for th in th_data:
-            z_fourier[th][m-1] = sol[rth_map[th]-1]
+        # cut off the initial condition, we don't want it here
+        z_fourier[:, m-1] = sol[0, 1:]
 
+    print('number of dst:', n)
     z_data = np.zeros(len(th_data))
 
-    def eval_z(i, th):
+    def eval_z(i, th=None):
+        if th is None:
+            th = th_data[i]
+
         z = 0
         for m in range(1, M+1):
             z += z_fourier[i, m-1] * np.sin(m*nu*(th-a))
@@ -69,8 +66,7 @@ def calc_z(problem, th_data, M):
         return z
 
     for i in range(len(th_data)):
-        for j in range(len(r_data)):
-            z_data[i] = eval_z(i)
+        z_data[i] = eval_z(i)
 
     # Calculate error incurred by ODE solver, for the last th value
     i = len(th_data)-1
